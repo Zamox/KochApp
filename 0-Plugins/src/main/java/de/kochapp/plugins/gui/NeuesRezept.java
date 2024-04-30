2f96cc88fa0b2aaf70918f7eb00e68c4be9615c9
Zamox <gesk.samuel.a21@student.dhbw-karlsruhe.de>
2024-04-30 04:17:42 +0200

    Refactoring: Ectract Method: NeuesRezept

--- 0-Plugins/src/main/java/de/kochapp/plugins/gui/NeuesRezept.java
+++ 0-Plugins/src/main/java/de/kochapp/plugins/gui/NeuesRezept.java
@@ -3,26 +3,38 @@ package de.kochapp.plugins.gui;
 import de.kochapp.adapter.Datenpersistenz.DataReader;
 import de.kochapp.adapter.GUIFunktionen.ButtonEditor;
 import de.kochapp.adapter.GUIFunktionen.ButtonRenderer;
-import de.kochapp.adapter.GUIFunktionen.FunktionenNeuesRezept;
 import de.kochapp.domain.Kategorie.Kategorie;
 import de.kochapp.domain.Kategorie.KategorieRepository;
-import de.kochapp.domain.Rezept.*;
+import de.kochapp.domain.Rezept.Schwierigkeit;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.List;
 
-/* Die KLasse erzeugt einen Frame, indem man ein neues Rezept hinzufügen kann*/
 public class NeuesRezept {
     final static KategorieRepository kategorieRepository = new KategorieRepository();
     JFrame frame = new JFrame();
     JPanel pnlNeuesRezept = new JPanel(new BorderLayout());
     JButton button = new JButton();
 
-    public NeuesRezept(DataReader dataReader)  {
+    public NeuesRezept(DataReader dataReader) {
+        initialisiereTitleUndTagsPanel(dataReader);
+        initialisiereZutatenPanel();
+        initialisiereBeschreibungPanel();
+
+        JPanel pnlUnten = new JPanel(new GridLayout(2,2));
+        initialisiereSchwierigkeitPanel(pnlUnten);
+
+        frame.add(pnlNeuesRezept);
+        frame.setVisible(true);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setBounds(400,100,1000,500);
+    }
+
+    private void initialisiereTitleUndTagsPanel(DataReader dataReader) {
         JPanel pnlMitte = new JPanel(new BorderLayout());
         JPanel pnlOben = new JPanel(new GridLayout(2,2));
         System.out.println("Das Panel für ein neues Rezept wird gestartet");
@@ -35,157 +47,77 @@ public class NeuesRezept {
         List<Kategorie> kategorien = kategorieRepository.findeAlleKategorien(dataReader.entityManager);
         JPanel pnlCheckboxen = new JPanel(new FlowLayout());
         Checkbox[] checkboxen = new Checkbox[kategorien.size()];
-        for(int i=0; i<kategorien.size(); i++){
+        for (int i = 0; i < kategorien.size(); i++) {
             Checkbox checkboxKategorie = new Checkbox(kategorien.get(i).bekommeKurzform());
-            checkboxen[i]= checkboxKategorie;
+            checkboxen[i] = checkboxKategorie;
             pnlCheckboxen.add(checkboxKategorie);
         }
-
+        pnlCheckboxen.setPreferredSize(new Dimension(400,150));
         pnlOben.add(labelTags);
-        pnlCheckboxen.setPreferredSize(new Dimension(600,120));
         pnlOben.add(pnlCheckboxen);
 
         pnlMitte.add(pnlOben, BorderLayout.NORTH);
+        pnlNeuesRezept.add(pnlMitte, BorderLayout.CENTER);
+    }
 
-        Einheit[] alleEinheiten = Einheit.values();
-        String [] stringEinheiten = new String[alleEinheiten.length];
-        for(int i=0; i<alleEinheiten.length; i++){
-            stringEinheiten[i] = alleEinheiten[i].bekommeBezeichnung();
-        }
-        JComboBox<String> comboBoxEinheit = new JComboBox<>(stringEinheiten);
-
-        JLabel labelZutaten = new JLabel("Zutaten: ");
-
+    private void initialisiereZutatenPanel() {
         JPanel pnlZutaten = new JPanel(new BorderLayout());
-
         DefaultTableModel model = new DefaultTableModel();
         JTable tabelle = new JTable(model);
-
         model.addColumn("Menge");
         model.addColumn("Einheit");
         model.addColumn("Zutat");
         model.addColumn("Löschen");
-
-        model.addRow(new Object[]{"","l", "", "-"});
+        model.addRow(new Object[]{"", "l", "", "-"});
 
         TableColumn einheitSpalte = tabelle.getColumnModel().getColumn(1);
-        einheitSpalte.setCellEditor(new DefaultCellEditor(comboBoxEinheit));
+        einheitSpalte.setCellEditor(new DefaultCellEditor(new JComboBox<>(new String[]{"l", "ml", "g"})));
         tabelle.getColumn("Löschen").setCellRenderer(new ButtonRenderer());
         tabelle.getColumn("Löschen").setCellEditor(new ButtonEditor(new JCheckBox()));
-        button.addActionListener(ae -> {
-            System.out.print("Button geklickt");
-            if(tabelle.getSelectedRow() != -1) {
-                // entferne die ausgeählte Zeile aus dem Model
-                model.removeRow(tabelle.getSelectedRow());
-                JOptionPane.showMessageDialog(null, "Die ausgewühlte Zeile würde erfolgreich gelöscht!");
-            }
-        });
-        tabelle.setMaximumSize(new Dimension(100,50));
+        tabelle.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        tabelle.setPreferredScrollableViewportSize(new Dimension(300, 150));
+
+
         pnlZutaten.add(new JScrollPane(tabelle), BorderLayout.CENTER);
         JButton buttonNeueZutat = new JButton("Zutat hinzufügen");
-        buttonNeueZutat.addActionListener(ae -> {
-            DefaultTableModel modelAusgangswerte = (DefaultTableModel) tabelle.getModel();
-            modelAusgangswerte.addRow(new Object[]{"", "l", "", "-"});
-        });
-
+        buttonNeueZutat.addActionListener(ae -> model.addRow(new Object[]{"", "l", "", "-"}));
         pnlZutaten.add(buttonNeueZutat, BorderLayout.SOUTH);
-        pnlZutaten.add(labelZutaten, BorderLayout.NORTH);
-        pnlZutaten.setPreferredSize(new Dimension(290,200));
-        pnlMitte.add(pnlZutaten, BorderLayout.EAST);
+        pnlNeuesRezept.add(pnlZutaten, BorderLayout.EAST);
+    }
 
+    private void initialisiereBeschreibungPanel() {
         JPanel pnlWesten = new JPanel(new BorderLayout());
         JLabel labelBeschreibung = new JLabel("Beschreibung: ");
         JTextArea textAreaBeschreibung = new JTextArea("Hier sollte die Beschreibung abc stehen");
+        textAreaBeschreibung.setPreferredSize(new Dimension(150, 350));
         textAreaBeschreibung.setLineWrap(true);
         textAreaBeschreibung.setWrapStyleWord(true);
         pnlWesten.add(labelBeschreibung, BorderLayout.NORTH);
         pnlWesten.add(new JScrollPane(textAreaBeschreibung), BorderLayout.CENTER);
-        pnlWesten.setPreferredSize(new Dimension(290,200));
-        pnlMitte.add(pnlWesten, BorderLayout.WEST);
+        pnlNeuesRezept.add(pnlWesten, BorderLayout.WEST);
+    }
 
-        JPanel pnlUnten = new JPanel(new GridLayout(2,2));
+    private void initialisiereSchwierigkeitPanel(JPanel pnlUnten) {
         JPanel pnlRadioButton = new JPanel(new FlowLayout());
         JLabel labelSchwierigkeitsgrad = new JLabel("Schwierigkeitsgrad: ");
         Schwierigkeit[] alleSchwierigkeitsgrade = Schwierigkeit.values();
         ButtonGroup gruppe = new ButtonGroup();
-        JRadioButton[] radiobuttons = new JRadioButton[alleSchwierigkeitsgrade.length];
-        for(int i=0; i<alleSchwierigkeitsgrade.length; i++){
-            JRadioButton radioButtonSchwierigkeiten = new JRadioButton(alleSchwierigkeitsgrade[i].toString());
-            radiobuttons[i]= radioButtonSchwierigkeiten;
-            gruppe.add(radiobuttons[i]);
-            pnlRadioButton.add(radiobuttons[i]);
+        for (Schwierigkeit schwierigkeit : alleSchwierigkeitsgrade) {
+            JRadioButton radioButton = new JRadioButton(schwierigkeit.toString());
+            gruppe.add(radioButton);
+            pnlRadioButton.add(radioButton);
         }
         pnlUnten.add(labelSchwierigkeitsgrad);
         pnlUnten.add(pnlRadioButton);
 
         JLabel labelDokument = new JLabel("Füge ein Dokument oder Bild hinzu ");
         JButton buttonDokument = new JButton("Wähle ein Dokument");
-        final String[] bildPfad = new String[1];
         buttonDokument.addActionListener(ae -> {
             FileChooser chooser = new FileChooser();
-            bildPfad[0] = chooser.dateiAuswahl();
+            chooser.dateiAuswahl();
         });
         pnlUnten.add(labelDokument);
         pnlUnten.add(buttonDokument);
-        pnlMitte.add(pnlUnten, BorderLayout.SOUTH);
-
-        JPanel pnlFusszeile = new JPanel(new FlowLayout());
-        JButton buttonAbbrechen = new JButton("Abbrechen");
-        buttonAbbrechen.addActionListener(ae -> frame.dispose());
-        JButton buttonSpeichern = new JButton("Speichern");
-        buttonSpeichern.addActionListener(ae -> {
-            boolean tabelleBearbeitet = false;
-            if(tabelle.getCellEditor() != null){
-                tabelle.getCellEditor().stopCellEditing();
-                tabelleBearbeitet = true;
-            }
-
-            String titel = textfeldTitel.getText();
-            String beschreibung = textAreaBeschreibung.getText();
-            String pfadBild = bildPfad[0];
-
-            ArrayList<String> checkedKategorien = new ArrayList<>();
-            for (Checkbox checkbox : checkboxen) {
-                if (checkbox.getState()) {
-                    checkedKategorien.add(checkbox.getLabel());
-                }
-            }
-
-            String ausewaehlteSchwierigkeit = "";
-            for (JRadioButton radiobutton : radiobuttons){
-                if (radiobutton.isSelected()){
-                    ausewaehlteSchwierigkeit = radiobutton.getText();
-                }
-            }
-
-            if(!titel.equals("") && !beschreibung.equals("") && tabelleBearbeitet && !checkedKategorien.isEmpty() && !ausewaehlteSchwierigkeit.equals("")) {
-                ArrayList<String[]> zutatenListe = new ArrayList<>();
-                for (int i = 0; i < tabelle.getRowCount(); i++) {
-                    String mengeText = (String) tabelle.getModel().getValueAt(i, 0);
-                    String einheitText = (String) tabelle.getModel().getValueAt(i, 1);
-                    String name = (String) tabelle.getModel().getValueAt(i, 2);
-                    String[] zutatEingabe = {mengeText, einheitText, name};
-                    zutatenListe.add(zutatEingabe);
-                }
-
-                FunktionenNeuesRezept.neuesRezeptSpeichern(titel, beschreibung, checkedKategorien,ausewaehlteSchwierigkeit, pfadBild, zutatenListe, dataReader);
-
-                frame.dispose();
-            }else{
-                JOptionPane.showMessageDialog(null, "Bitte füllen Sie alle Felder aus!");
-            }
-        });
-
-        pnlFusszeile.add(buttonAbbrechen);
-        pnlFusszeile.add(buttonSpeichern);
-        pnlNeuesRezept.add(pnlMitte, BorderLayout.CENTER);
-        pnlNeuesRezept.add(pnlFusszeile, BorderLayout.SOUTH);
-
-        frame.add( pnlNeuesRezept );
-        frame.setVisible(true);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setBounds(400,100,600,700);
+        pnlNeuesRezept.add(pnlUnten, BorderLayout.SOUTH);
     }
-
 }
